0d7deed66f7c66be58b8a3bb3d2b06b710df3e5b
Eddie <zxc1435693@gmail.com>
2023-01-09 14:32:51 +0800

    Add annotation about GoF

--- 3.Behavioral/src/Observer_Podcast/Test.java
+++ 3.Behavioral/src/Observer_Podcast/Test.java
@@ -5,8 +5,8 @@ import Observer_Podcast.ConcreteSubject.PodcastA;
 
 public class Test {
     public static void main(String[] args) {
-        Subject podcast = new PodcastA();
-        Observer student = new Student(podcast);
+        Subject podcast = new PodcastA(); //Concrete Subject
+        Observer student = new Student(podcast); //Concrete Observer
         podcast.add(student);
 
         //預設節目是英文廣播
